8a5c68d1d6bacbffc9987b4807af9ec985b48021
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-11-03 10:59:40 +0100

    Finish Phonebook backend step 4

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.get("/api/persons/:id", (req, res) => {
 	}
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	persons = persons.filter(person => person.id !== id);
+
+	res.status(204).end();
+});
+
 app.get("/info", (req, res) => {
 	let date = new Date();
 	res.send(`<p>Phonebook has info for ${persons.length} people</p>
