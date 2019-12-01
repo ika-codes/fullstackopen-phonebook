6f52a14282e42de76e82162c43b0aec3e6813f7a
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2019-12-01 12:26:05 +0100

    Finish Phonebook backend step 7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var morgan = require("morgan");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
 let persons = [
 	{
